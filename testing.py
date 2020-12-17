0d153dab51c62a2b8530d52d3f3560c910ad76c0
Bront <Brentholt_1@outlook.com>
2020-12-17 00:17:27 -0500

    added testing to gitignore

--- testing.py
+++ testing.py
@@ -10,12 +10,12 @@ disrank = generator.Generator()
 args = {
 			'bg_image' : '', # Background image link (Optional)
 			'profile_image' : 'https://images-ext-1.discordapp.net/external/Vn2YDNmin4oNdyO91g9N1ss5i_VyZoCwP1YyesAlEkA/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/98802238364876800/a8730866ce43c5e5988064977b4c1849.png', # User profile picture link
-			'level' : 100, # User current level 
+			'level' : 1, # User current level 
 			'current_xp' : 10, # Current level minimum xp 
 			'user_xp' : 50, # User current xp
 			'next_xp' : 100, # xp required for next level
 			'user_position' : 1, # User position in leaderboard
-			'user_name' : "DiabeticTurtle#1604", # user name with descriminator 
+			'user_name' : "cotufa82 | LADAmme#1604", # user name with descriminator 
 			'user_status' : "online", # User status eg. online, offline, idle, streaming, dnd
 		}
 
